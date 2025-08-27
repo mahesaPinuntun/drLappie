491beb6212cfe729ce11d490e4e5ca3840099c38
mahesaPinuntun <140386522+mahesaPinuntun@users.noreply.github.com>
2025-08-28 04:44:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -431,5 +431,5 @@ def cekpotensikangker():
     #app.run(debug=False)
 
 import os
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 8080))
 app.run(host="0.0.0.0", port=port)
